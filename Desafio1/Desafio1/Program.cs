68089321c8c8946f1f0ad66d4f8919f993180880
PEGADURO\jorgeav <analista@pegaduro.com.mx>
2024-09-28 13:55:00 -0600

    no message

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string miString;
+
+            Console.WriteLine("Por favor, ingresa tu nombre y presiona enter: ");
+            miString = Console.ReadLine();
+
+            Console.WriteLine(miString.ToUpper());
+            Console.WriteLine(miString.ToLower());
+            Console.WriteLine(miString.Trim());
+            Console.WriteLine(miString.Substring(3));
+
+            Console.Read();
+        }
+    }
+}
