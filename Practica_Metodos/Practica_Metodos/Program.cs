f62917755b58f20f2e545b63ada81b2ac04abf59
PEGADURO\jorgeav <analista@pegaduro.com.mx>
2024-09-28 13:57:13 -0600

    no message

---
+++ Practica_Metodos/Practica_Metodos/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Multiplicar(15, 14));
+            Console.Read();
+        }
+
+        #region Metodos sin valor de retorno
+        public static void Escribe() 
+        {
+            Console.WriteLine("Escribiendo desde el metodo.");            
+        }
+
+        public static void EscibeEspicifico(string texto) 
+        {
+            Console.WriteLine(texto);            
+        }
+        #endregion
+
+        #region Metodos con valor de retorno
+        public static int Sumar(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public static int Multiplicar(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        #endregion
+    }
+}
